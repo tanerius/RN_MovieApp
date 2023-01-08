b13c4b465f17e0cb4e4bfde3644599cbeae1c30a
Taner Selim <taner.selim@Tanerss-MacBook-Air.local>
2023-01-08 22:01:26 +0100

    Implemented the stack navigator in App.js

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {View} from 'react-native';
 import Home from './components/screens/Home';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Home></Home>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 export default App;
