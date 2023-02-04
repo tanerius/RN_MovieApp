1a288289303ae62af0bc30a9673fa3fd2c5cdcdb
Taner Selim <tanerius@gmail.com>
2023-02-04 11:44:11 +0100

    Implemented the name and genre list for movie details

--- components/screens/Detail.js
+++ components/screens/Detail.js
@@ -5,6 +5,8 @@ import {
   Image,
   Dimensions,
   ActivityIndicator,
+  Text,
+  View,
 } from 'react-native';
 import {getMovie} from '../../services/services';
 
@@ -38,6 +40,20 @@ const Detail = ({route, navigation}) => {
                   }
                 : placeholderImage
             }></Image>
+          <View style={styles.container}>
+            <Text style={styles.movieTitle}>{movieDetail.title}</Text>
+            {movieDetail.genres && (
+              <View style={styles.movieGenresContainer}>
+                {movieDetail.genres.map(genreItem => {
+                  return (
+                    <Text style={styles.movieGenreItem} key={genreItem.id}>
+                      {genreItem.name} ({genreItem.id})
+                    </Text>
+                  );
+                })}
+              </View>
+            )}
+          </View>
         </ScrollView>
       )}
       {!loaded && <ActivityIndicator size="large" />}
@@ -48,6 +64,26 @@ const Detail = ({route, navigation}) => {
 const styles = StyleSheet.create({
   movieImage: {
     height: height / 2,
+  },
+  movieTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  movieGenresContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+  },
+  movieGenreItem: {
+    marginRight: 10,
+    fontWeight: 'normal',
+    marginTop: 20,
   },
 });
 
