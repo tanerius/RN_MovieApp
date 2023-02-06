ed7b917f45f4b72213c28080657a90a92b918ab1
Taner Selim <tanerius@gmail.com>
2023-02-06 01:17:02 +0100

    Refactored the main navigation of app to MainNav component

--- App.js
+++ App.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import {View} from 'react-native';
-import Home from './components/screens/Home';
-import Detail from './components/screens/Detail';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Navbar from './components/Navbar';
-
-const Stack = createNativeStackNavigator();
+import MainNav from './components/MainNav';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerTransparent: true,
-            header: ({navigation}) => (
-              <Navbar navigation={navigation} main={true} />
-            ),
-          }}
-        />
-        {/* Add a details screen */}
-        <Stack.Screen
-          name="Detail"
-          component={Detail}
-          options={{
-            headerTransparent: true,
-            header: ({navigation}) => (
-              <Navbar navigation={navigation} main={false} />
-            ),
-          }}
-        />
-      </Stack.Navigator>
+      <MainNav />
     </NavigationContainer>
   );
 };
---
+++ components/MainNav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Home from './screens/Home';
+import Detail from './screens/Detail';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Navbar from './Navbar';
+
+const Stack = createNativeStackNavigator();
+
+class MainNav extends React.PureComponent {
+  render() {
+    return (
+      <Stack.Navigator headerMode={'screen'}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTransparent: true,
+            header: ({navigation}) => (
+              <Navbar navigation={navigation} main={true} />
+            ),
+          }}
+        />
+        {/* Add a details screen */}
+        <Stack.Screen
+          name="Detail"
+          component={Detail}
+          options={{
+            headerTransparent: true,
+            header: ({navigation}) => (
+              <Navbar navigation={navigation} main={false} />
+            ),
+          }}
+        />
+      </Stack.Navigator>
+    );
+  }
+}
+
+export default MainNav;
